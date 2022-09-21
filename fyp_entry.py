4c11caadd2f8ef5bb5481509bfa5f4d33dd315a3
kaishengmar <114069872+kaishengmar@users.noreply.github.com>
2022-09-21 23:27:13 +0800

    Update fyp_entry.py

--- fyp_entry.py
+++ fyp_entry.py
@@ -5,4 +5,4 @@ HEADER = "Crime Dataset Captioning"
 st.header(HEADER, anchor=None)
 st.text("Testing 1")
 st.info('Please upload your file below! Make sure it is an MP4 format!')
-st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
