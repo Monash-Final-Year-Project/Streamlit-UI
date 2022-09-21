ae434507d8dcba9564c3420ab317c88cd93aaf7e
kaishengmar <114069872+kaishengmar@users.noreply.github.com>
2022-09-21 23:23:40 +0800

    Update fyp_entry.py

--- fyp_entry.py
+++ fyp_entry.py
@@ -3,5 +3,5 @@ import streamlit as st
 HEADER = "Crime Dataset Captioning"
 
 st.header(HEADER, anchor=None)
-st.text("Testing testting 1")
+st.text("Testing 1")
 st.info('Please upload your file below! Make sure it is an MP4 format!')
