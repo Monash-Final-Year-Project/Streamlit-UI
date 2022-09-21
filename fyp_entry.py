d2f130f1ea521a81a088141dcd20db41458616a0
kaishengmar <114069872+kaishengmar@users.noreply.github.com>
2022-09-21 23:31:04 +0800

    Update fyp_entry.py

--- fyp_entry.py
+++ fyp_entry.py
@@ -1,8 +1,12 @@
 import streamlit as st
+from PIL import Image
 
 HEADER = "Crime Dataset Captioning"
 
 st.header(HEADER, anchor=None)
 st.text("Testing 1")
 st.info('Please upload your file below! Make sure it is an MP4 format!')
-st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+
+
+image = Image.open('asta.jpeg')
+st.image(image, caption='Asta')
