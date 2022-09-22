ea8442d296b273b359fcc3f749eb2016d4c9d89c
kaishengmar <kmar0047@student.monash.edu>
2022-09-22 22:04:00 +0800

    Added new code for UI

--- fyp_entry.py
+++ fyp_entry.py
@@ -1,10 +1,26 @@
 import streamlit as st
 
-HEADER = "Crime Dataset Captioning"
 
-st.header(HEADER, anchor=None)
-st.text("Testing 1")
-st.info('Please upload your file below! Make sure it is an MP4 format!')
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick an video to test')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.video(image_data)
+
+
+def main():
+    st.title('Crime Dataset Captioning')
+    st.info('Please upload your video below! Make sure it is in MP4 format!')
+    load_image()
+    st.button('Run model to caption the video')
+    st.text("Frame 0 to 9 seconds:")
+    st.text("Pedestrians walking across the road normally.")
+    st.text("Frame 10 to 19 seconds:")
+    st.text("A man gets hit in the back of the head by another pedestrian.")
+    st.text("Frame 20 to 26 seconds:")
+    st.text("The victim is knocked out and other pedestrians are rushing to help him.")
 
 
 
+if __name__ == '__main__':
+    main()
